75c52a056771a830299b18539c6d22f8c5b0de7e
Suzanne Millstein <smillst@cs.washington.edu>
2016-04-27 12:52:38 -0700

    Fix formating

--- framework/src/org/checkerframework/common/wholeprograminference/WholeProgramInferenceScenesHelper.java
+++ framework/src/org/checkerframework/common/wholeprograminference/WholeProgramInferenceScenesHelper.java
@@ -515,7 +515,7 @@ public class WholeProgramInferenceScenesHelper {
             // So instead, only insert the annotation if there is not primary annotation of the same hierarchy.
             // #shouldIgnore prevent annotations that are subtypes of type vars upper bound from being inserted.
             for (AnnotationMirror am : newATM.getAnnotations()) {
-                if(curATM.getAnnotationInHierarchy(am) != null) {
+                if (curATM.getAnnotationInHierarchy(am) != null) {
                     // Don't insert if the type is already has a primary annotation
                     // in the same hierarchy.
                     break;
